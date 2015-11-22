5958d41aebb4f571e15160113a18ab21ba23a37a
Matthew Marji <matthew.marji@utoronto.ca>
2015-11-22 15:14:43 -0500

    Initial Check-In for Product Info Backend
    - This will be home to the backend infrastructure needed to retrieve
    product information.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  #protect_from_forgery with: :exception
+
+    def product_information
+        puts params[:product_id]
+
+        id = params[:product_id]
+
+        if id == 1
+            render json: {product_name:"Special K"}, status: 200
+        elsif id == 2
+            render json: {product_name:"Lucky Charms"}, status: 200
+        elsif id == 3
+            render json: {product_name:"Corn Pops"}, status: 200
+        end
+    end
+end
