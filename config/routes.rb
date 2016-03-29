56a74c8ff1af6a49ee03faa159e85f2d59b50494
Matthew Marji <matthew.marji@utoronto.ca>
2016-03-29 12:10:20 -0400

    Open GET endpoint to /product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
 
+    get '/product', to: 'products#show'
     post '/product', to: 'products#show'
     get '/ping', to: 'application#ping'
 end
